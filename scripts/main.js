8c33f21f99a9313c47c476ebe7e706782abd7450
milton <milton1pereira@gmail.com>
2019-10-09 19:46:48 +0530

    added more input box

--- scripts/main.js
+++ scripts/main.js
@@ -7,5 +7,14 @@ function validateForm() {
     alert("Name must be filled out");
     return false;
   }
-}
+},
 
+
+function myFunction() {
+  var inpObj = document.getElementById("id1");
+  if (!inpObj.checkValidity()) {
+    document.getElementById("demo").innerHTML = inpObj.validationMessage;
+  } else {
+    document.getElementById("demo").innerHTML = "Input OK";
+  } 
+} 
