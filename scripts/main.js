806e1370cfa98b5fe9f333483e42032d91a81f99
milton <milton1pereira@gmail.com>
2019-10-09 19:09:19 +0530

    added field

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+
